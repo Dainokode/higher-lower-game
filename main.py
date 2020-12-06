b709e7a344536d5059bc8362eb04bca840f028bc
Dainokode <antoniodainok@gmail.com>
2020-12-06 14:15:27 +0100

    new version less code

--- main.py
+++ main.py
@@ -1,55 +1,55 @@
-# imports
 from art import *
 from data import data
 import random
 
 
 def random_account():
-    """Returns a random account from data list"""
-    return random.choice(data)
+    """return random account from data list"""
+    account = random.choice(data)
+    return account
 
 
 def print_accounts(a, b):
+    """prints accounts info"""
+    # test
+    print(a["follower_count"])
+    print(b["follower_count"])
+
     print(f"Compare A: {a['name']} a {a['description']} from {a['country']}")
     print(vs)
-    print(f"Against B: {b['name']} a {b['description']} from {b['country']}\n")
+    print(f"Compare B: {b['name']} a {b['description']} from {b['country']}")
+
+
+def compare_accounts(a, b):
+    score = 0
+    game_over = False
+    while not game_over:
+        a_followers = a["follower_count"]
+        b_followers = b["follower_count"]
+
+        print_accounts(a, b)
+
+        guess = input("Who has more followers? 'A' or 'B': ").lower()
+
+        if a_followers > b_followers and guess == "a" or b_followers > a_followers and guess == "b":
+            score += 1
+            print(f"Correct, your score is {score}")
+
+            if a_followers > b_followers:
+                a = random_account()
+                print_accounts(b, a)
+            else:
+                b = random_account()
+                print_accounts(a, b)
+        else:
+            print(f"Sorry, you lost. Your final score is {score}")
+            game_over = True
 
 
 print(logo)
 
-account_a = random_account()
-account_b = random_account()
-# print(account_a['follower_count'])
-# print(account_b['follower_count'])
+# pick accounts
+a = random_account()
+b = random_account()
 
-print_accounts(account_a, account_b)
-
-user_choice = input("Who has more followers? Type 'A' or 'B': ").lower()
-
-game_over = False
-score = 0
-while game_over == False:
-    if user_choice == "a" and account_a["follower_count"] > account_b["follower_count"] or user_choice == "b" and account_b["follower_count"] > account_a["follower_count"]:
-        score += 1
-        print(f"You're right! Your score is: {score}\n")
-
-        account_loser = None
-
-        if account_a["follower_count"] < account_b["follower_count"]:
-            account_loser = account_a
-        else:
-            account_loser = account_b
-
-        account_a = random_account()
-        account_b = random_account()
-
-        # print(account_loser['follower_count'])
-        # print(account_b['follower_count'])
-
-        print_accounts(account_loser, account_b)
-
-        user_choice = input(
-            "Who has more followers? Type 'A' or 'B': ").lower()
-    else:
-        print(f"Sorry, that's wrong. Your final score is {score}")
-        game_over = True
+compare_accounts(a, b)
