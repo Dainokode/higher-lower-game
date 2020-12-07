36cf690de247c4931fd53f6ddeb9f65d215ada0c
Dainokode <antoniodainok@gmail.com>
2020-12-07 12:07:37 +0100

    final commit

--- main.py
+++ main.py
@@ -20,27 +20,28 @@ def print_accounts(a, b):
     print(f"Compare B: {b['name']} a {b['description']} from {b['country']}")
 
 
-def compare_accounts(a, b):
+def compare(a, b):
     score = 0
     game_over = False
     while not game_over:
+        # followers
         a_followers = a["follower_count"]
         b_followers = b["follower_count"]
-
+        # print accounts info - a vs b
         print_accounts(a, b)
-
-        guess = input("Who has more followers? 'A' or 'B': ").lower()
-
-        if a_followers > b_followers and guess == "a" or b_followers > a_followers and guess == "b":
+        # prompt user
+        guess = input("Who has more followers? 'A' or 'B'? ")
+        # game logic
+        if a_followers > b_followers and guess == "a":
             score += 1
-            print(f"Correct, your score is {score}")
-
-            if a_followers > b_followers:
-                a = random_account()
-                print_accounts(b, a)
-            else:
-                b = random_account()
-                print_accounts(a, b)
+            print(f"Correct! Your score is {score}")
+            a = b
+            b = random_account()
+        elif b_followers > a_followers and guess == "b":
+            score += 1
+            print(f"Correct! Your score is {score}")
+            a = b
+            b = random_account()
         else:
             print(f"Sorry, you lost. Your final score is {score}")
             game_over = True
@@ -48,8 +49,7 @@ def compare_accounts(a, b):
 
 print(logo)
 
-# pick accounts
 a = random_account()
 b = random_account()
 
-compare_accounts(a, b)
+compare(a, b)
